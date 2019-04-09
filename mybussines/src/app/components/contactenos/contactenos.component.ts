c99bf8b4497220036d768eef7193d5650b678fc3
greivin24 <31831690+greivin24@users.noreply.github.com>
2019-04-09 16:54:56 -0500

    Arreglando pequeños detallas de vistas de villa

--- mybussines/src/app/components/contactenos/contactenos.component.ts
+++ mybussines/src/app/components/contactenos/contactenos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FirebaseService } from '../../services/firebase.service';
+
+import * as AOS from 'aos';
+import * as alertify from 'alertifyjs';
 
 @Component({
   selector: 'app-contactenos',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactenosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseService:FirebaseService) { }
 
   ngOnInit() {
+    AOS.init();
+  }
+
+  btnEnviarMsj(val:NgForm){
+    const data ={
+      "nombre": val.value.nombre,
+      "email": val.value.email,
+      "mensaje": val.value.msj,
+    }
+    this.firebaseService.post(data, "casillero").subscribe (result =>{
+      console.log(result);
+      alertify.alert("Mensaje recibido exitosamente, pronto estaremos en contacto.");
+      val.reset();
+    })
   }
 
 }
